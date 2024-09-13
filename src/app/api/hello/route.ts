4d97fa7287b9c1b427b0e46806e36a0c8b6a7e83
henzyd <henzydee@gmail.com>
2024-09-13 07:42:56 +0100

    drizzle setup complete

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
+import { db } from "~/db";
+import { users } from "~/db/schema";
 
 export async function GET() {
+  const allUsers = await db.select().from(users);
+
   return NextResponse.json({
-    data: "Hello World",
+    data: allUsers,
+  });
+}
+
+export async function POST() {
+  await db.insert(users).values({ fullName: "uchechukwu anachuna", phone: "+2345958303" });
+
+  return NextResponse.json({
+    meesage: "success",
   });
 }
