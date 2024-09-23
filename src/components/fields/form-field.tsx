5590d50840d2c51f15f21f044dacb8a7ff703f85
henzyd <henzydee@gmail.com>
2024-09-23 06:58:49 +0100

    feat: create login page ui

--- src/components/fields/form-field.tsx
+++ src/components/fields/form-field.tsx
@@ -1,32 +1,80 @@
-import { TextField, TextFieldProps } from "@mui/material";
+"use client";
+
+import { useState } from "react";
+import {
+  IconButton,
+  InputLabel,
+  InputLabelProps,
+  OutlinedInput,
+  OutlinedInputProps,
+} from "@mui/material";
 import { ErrorMessage, Field, FieldConfig, FieldProps } from "formik";
+import { BsEyeSlash } from "react-icons/bs";
+import { AiOutlineEye } from "react-icons/ai";
 import { cn } from "~/utils/helpers";
 
-type Props = TextFieldProps &
+type Props = OutlinedInputProps &
   FieldConfig & {
     wrapperClassName?: string;
+    labelProps?: InputLabelProps;
   };
 
 export default function FormField({
   className,
   wrapperClassName,
   color,
-  required: _,
+  required,
+  sx,
+  label,
+  type,
+  labelProps,
   ...props
 }: Props) {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <Field {...props}>
-      {({ field, meta }: FieldProps) => (
+      {({ field }: FieldProps) => (
         <div className={cn("flex w-full flex-col gap-1", wrapperClassName)}>
-          <TextField
+          {label && (
+            <div className="flex w-full items-center justify-between">
+              <InputLabel
+                {...labelProps}
+                htmlFor={props.name}
+                className={cn(`!text-xs`, labelProps?.className)}
+              >
+                {label}
+                {required && <span className="pl-1 !text-xs !text-red-600">*</span>}
+              </InputLabel>
+            </div>
+          )}
+          <OutlinedInput
+            className={cn("w-full", className)}
             {...field}
             {...props}
-            className={cn(className)}
             sx={{
+              input: {
+                fontSize: "0.8rem",
+                "::placeholder": {
+                  fontSize: "0.8rem",
+                },
+              },
               "& .MuiOutlinedInput-input": {
-                borderColor: `${props.InputProps?.readOnly ? "transparent" : "#0071B9"}`,
+                padding: "11px 13.5px",
               },
             }}
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              type === "password" && (
+                <IconButton
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  edge="end"
+                  size="small"
+                >
+                  {showPassword ? <BsEyeSlash /> : <AiOutlineEye />}
+                </IconButton>
+              )
+            }
           />
           <ErrorMessage
             name={props.name}
