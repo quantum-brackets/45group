7df9c5e4b3c5558712bf6289a8776209456a48ed
henzyd <henzydee@gmail.com>
2024-09-23 04:58:53 +0100

    updated select input

--- src/components/inputs/select-input.tsx
+++ src/components/inputs/select-input.tsx
@@ -1,30 +1,44 @@
-import { FormControl, FormControlProps, InputLabel, Select, SelectProps } from "@mui/material";
+import { InputLabel, InputLabelProps, Select, SelectProps } from "@mui/material";
 import { cn } from "~/utils/helpers";
 
 type Props = SelectProps & {
-  formControl?: FormControlProps;
+  labelProps?: InputLabelProps;
 };
 
-export default function SelectInput({ children, sx, className, label, ...props }: Props) {
+export default function SelectInput({
+  children,
+  sx,
+  className,
+  label,
+  labelProps,
+  ...props
+}: Props) {
   return (
-    <Select
-      displayEmpty
-      {...props}
-      className={cn(className)}
-      sx={{
-        "& .MuiFilledInput-input": {
-          borderTop: "0px",
-          borderRight: "0px",
-          borderLeft: "0px",
-          borderWidth: "2px",
-          borderColor: "#0e82bb !important",
-          borderRadius: "4px",
-          fontSize: "12px",
-        },
-        ...sx,
-      }}
-    >
-      {children}
-    </Select>
+    <div className="flex flex-col gap-2">
+      {label && (
+        <InputLabel {...labelProps} className="!text-sm !font-semibold">
+          {label}
+        </InputLabel>
+      )}
+      <Select
+        displayEmpty
+        {...props}
+        className={cn(className)}
+        sx={{
+          "& .MuiFilledInput-input": {
+            borderTop: "0px",
+            borderRight: "0px",
+            borderLeft: "0px",
+            borderWidth: "2px",
+            borderColor: "#0e82bb !important",
+            borderRadius: "4px",
+            fontSize: "12px",
+          },
+          ...sx,
+        }}
+      >
+        {children}
+      </Select>
+    </div>
   );
 }
