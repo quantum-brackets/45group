542fde4e11c2f2a2cf2e1aee0664f4a7ff08fdc5
henzyd <henzydee@gmail.com>
2024-09-29 13:52:01 +0100

    fix: search

--- src/app/(core)/booking/layout.tsx
+++ src/app/(core)/booking/layout.tsx
@@ -1,15 +1,26 @@
 import { ReactNode, Suspense } from "react";
+import { Skeleton } from "@mui/material";
 import Filters from "~/modules/core-layout/booking-layout/filters";
 import Header from "~/modules/core-layout/booking-layout/header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto flex max-w-1700 flex-grow tablet:flex-col">
+    <div className="flex w-full max-w-1700 flex-grow self-center tablet:flex-col">
       <Suspense>
         <Filters />
       </Suspense>
-      <main className="w-[calc(100%-300px)] tablet:!w-full largeTabletAndBelow:w-[calc(100%-250px)]">
-        <Suspense>
+      <main className="w-[calc(100%-250px)] tablet:!w-full">
+        <Suspense
+          fallback={
+            <header className="flex w-full items-center justify-between gap-8 border-b-1.5 p-2 px-8 tablet:px-4">
+              <Skeleton variant="rounded" className="h-[20px] w-[100px]" />
+              <div className="flex items-center gap-4">
+                <Skeleton variant="rounded" className="!h-[40px] w-[300px]" />
+                <Skeleton variant="rounded" className="!h-[40px] w-[180px]" />
+              </div>
+            </header>
+          }
+        >
           <Header />
         </Suspense>
         {children}
