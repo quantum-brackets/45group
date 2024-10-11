1b15f645edb036296651be0caf6dc6e1c04fd6dc
henzyd <henzydee@gmail.com>
2024-10-11 16:01:30 +0100

    chore: created request and verify otp endpoints

--- src/db/schemas/users.ts
+++ src/db/schemas/users.ts
@@ -1,4 +1,4 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, json, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -6,6 +6,8 @@ export const users = pgTable("users", {
   last_name: varchar("last_name", { length: 100 }),
   email: varchar("email", { length: 320 }).unique().notNull(),
   phone: varchar("phone", { length: 256 }).unique(),
+  is_verified: boolean("is_verified").default(false),
   updated_at: timestamp("updated_at"),
   created_at: timestamp("created_at").defaultNow(),
+  metadata: json("metadata"),
 });
