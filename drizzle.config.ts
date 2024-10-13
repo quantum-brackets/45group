c218c02afdc4097e9d128a0cfecd5d683b251534
henzyd <henzydee@gmail.com>
2024-10-13 18:18:24 +0100

    fix: ci bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-if (process.env.NODE_ENV === "production") {
-  dotenv.config({ path: ".env.production" });
-} else {
+if (process.env.NODE_ENV === "development") {
   dotenv.config({ path: ".env.local" });
+} else {
+  dotenv.config({ path: ".env" });
 }
 
 export default defineConfig({
