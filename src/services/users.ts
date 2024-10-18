ae8cc3b44a506bd904145d4d83923f0f9e47bd0d
henzyd <henzydee@gmail.com>
2024-10-18 16:53:34 +0100

    chore: created PATCH and POST methods for /users/me endpoint

--- src/services/users.ts
+++ src/services/users.ts
@@ -2,7 +2,13 @@ import { axiosPrivate } from "~/config/axios";
 
 class UsersService {
   static updateMe = async (data: any) => {
-    const { data: response } = await axiosPrivate.post<any>(`/api/users/me`, data);
+    const { data: response } = await axiosPrivate.post<User>(`/api/users/me`, data);
+
+    return response;
+  };
+
+  static getMe = async () => {
+    const { data: response } = await axiosPrivate.get<User>(`/api/users/me`);
 
     return response;
   };
