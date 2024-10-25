934a88bc5e0f5716707eed8500aada3f24e95caf
henzyd <henzydee@gmail.com>
2024-10-25 17:24:45 +0100

    removed svgr

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["mui-tel-input"],
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /fc/] },
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: /fc/,
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {
