5b281e42c71ce00607e28e3860c28014bb16f5cd
henzyd <henzydee@gmail.com>
2024-10-30 14:39:07 +0100

    integrated next auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,58 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import { and, eq } from "drizzle-orm";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "~/db";
+import { usersTable } from "~/db/schemas/users";
+import { COOKIE_MAX_AGE, SESSION_KEY } from "~/utils/constants";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email) return null;
+
+        const [user] = await db
+          .select()
+          .from(usersTable)
+          .where(and(eq(usersTable.email, credentials.email), eq(usersTable.is_verified, true)));
+
+        if (!user) return null;
+
+        return {
+          id: user.id,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: COOKIE_MAX_AGE,
+    generateSessionToken() {
+      return SESSION_KEY;
+    },
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = {
+        id: token.id,
+      };
+
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const COOKIE_MAX_AGE = 30 * 24 * 60 * 60 * 1000; // 30  days
+export const COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000; // 7 days
 
 export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?^&£#.])[A-Za-z\d@$!%*?^&£#.]{8,}$/;
@@ -6,6 +6,8 @@ export const passwordRegex =
 export const DEFAULT_CURRENCY_CODE = "ngn";
 
 export const JWT_KEY = "_jwt";
+
+export const SESSION_KEY = "_session";
 
 // export const phoneRegExp = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
 export const phoneRegExp = /^[0-9]{11}$/;
