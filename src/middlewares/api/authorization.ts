c468ab9fc695fda0327ebad1dcedcc9f1b5dd2ab
henzyd <henzydee@gmail.com>
2024-10-30 16:39:24 +0100

    fix: authorization

--- src/middlewares/api/authorization.ts
+++ src/middlewares/api/authorization.ts
@@ -1,10 +1,9 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
+import { decode } from "next-auth/jwt";
 import { MiddlewareFactory } from "../stack-middlewares";
 import { appError } from "~/utils/helpers";
 import catchAsync from "~/utils/catch-async";
 import { HEADER_DATA_KEY, SESSION_KEY } from "~/utils/constants";
-import { decode } from "next-auth/jwt";
 
 const protectedRoutes = ["/api/users"];
 
@@ -13,8 +12,8 @@ export const authorization: MiddlewareFactory = (next) => {
     const pathname = req.nextUrl.pathname;
 
     if (protectedRoutes.some((path) => pathname.startsWith(path))) {
-      const session =
-        req.cookies.get(SESSION_KEY)?.value || req.headers.get("Authorization")?.split(" ")[1];
+      const session = req.headers.get("Authorization")?.split(" ")[1];
+      // req.cookies.get(SESSION_KEY)?.value || req.headers.get("Authorization")?.split(" ")[1];
 
       const data = await decode({
         secret: process.env.NEXTAUTH_SECRET!,
