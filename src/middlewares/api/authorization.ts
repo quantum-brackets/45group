dfb4f3cda0e1e8d7260503365bc433f42c48466c
henzyd <henzydee@gmail.com>
2024-10-30 18:29:25 +0100

    fix: authorization

--- src/middlewares/api/authorization.ts
+++ src/middlewares/api/authorization.ts
@@ -10,26 +10,26 @@ const protectedRoutes = ["/api/users"];
 export const authorization: MiddlewareFactory = (next) => {
   return catchAsync(async (req: NextRequest, _next: NextFetchEvent) => {
     const pathname = req.nextUrl.pathname;
-    const sessionToken =
-      req.cookies.get(SESSION_KEY)?.value || req.headers.get("Authorization")?.split(" ")[1];
+    // const sessionToken =
+    //   req.cookies.get(SESSION_KEY)?.value || req.headers.get("Authorization")?.split(" ")[1];
 
-    if (protectedRoutes.some((path) => pathname.startsWith(path))) {
-      if (!sessionToken) {
-        return appError({ status: 401, error: "No session provided" });
-      }
+    // if (protectedRoutes.some((path) => pathname.startsWith(path))) {
+    //   if (!sessionToken) {
+    //     return appError({ status: 401, error: "No session provided" });
+    //   }
 
-      const {
-        data: { user_id },
-      } = await axiosInstance.post("/api/utils/decode", { session: sessionToken });
+    //   const {
+    //     data: { user_id },
+    //   } = await axiosInstance.post("/api/utils/decode", { session: sessionToken });
 
-      if (!user_id) {
-        return appError({ status: 401, error: "Invalid session" });
-      }
+    //   if (!user_id) {
+    //     return appError({ status: 401, error: "Invalid session" });
+    //   }
 
-      const res = NextResponse.next();
-      res.headers.set(HEADER_DATA_KEY, user_id);
-      return res;
-    }
+    //   const res = NextResponse.next();
+    //   res.headers.set(HEADER_DATA_KEY, user_id);
+    //   return res;
+    // }
     return next(req, _next);
   });
 };
