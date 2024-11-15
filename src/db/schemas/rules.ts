ca51184e02a4e3c89e9a0dd82dbfe54533eda484
henzyd <henzydee@gmail.com>
2024-11-15 17:03:47 +0100

    updated models

--- src/db/schemas/rules.ts
+++ src/db/schemas/rules.ts
@@ -1,6 +1,4 @@
-import { relations } from "drizzle-orm";
 import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
-import { lodgesRulesTable } from "./lodges";
 
 export const rulesTable = pgTable("rules", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -10,7 +8,3 @@ export const rulesTable = pgTable("rules", {
   updated_at: timestamp("updated_at"),
   created_at: timestamp("created_at").defaultNow(),
 });
-
-export const rulesRelations = relations(rulesTable, ({ many }) => ({
-  lodges_rules: many(lodgesRulesTable),
-}));
