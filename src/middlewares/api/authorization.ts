c0410383b152d5f9088d767b6ba36e24f1cab289
henzyd <henzydee@gmail.com>
2024-12-11 18:23:18 +0100

    clean up

--- src/middlewares/api/authorization.ts
+++ src/middlewares/api/authorization.ts
@@ -1,8 +1,8 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { NextFetchEvent, NextRequest } from "next/server";
 import { MiddlewareFactory } from "../stack-middlewares";
 import { appError } from "~/utils/helpers";
 import catchAsync from "~/utils/catch-async";
-import { HEADER_DATA_KEY, SESSION_KEY } from "~/utils/constants";
+import { SESSION_KEY } from "~/utils/constants";
 import axiosInstance from "~/config/axios";
 
 const protectedRoutes = ["/api/users", "/api/auth/logout", "/api/auth/set-email"];
@@ -10,19 +10,7 @@ const protectedRoutes = ["/api/users", "/api/auth/logout", "/api/auth/set-email"
 export const authorization: MiddlewareFactory = (next, _, data) => {
   return catchAsync(async (req: NextRequest, _next: NextFetchEvent) => {
     const pathname = req.nextUrl.pathname;
-    const sessionToken =
-      req.cookies.get(SESSION_KEY)?.value ||
-      req.headers.get("Authorization")?.split(" ")[1] ||
-      req.headers.get("authorization")?.split(" ")[1];
-
-    // Method 2: Using forEach
-    req.headers.forEach((value, key) => {
-      console.log("in middleware");
-      console.log(key, value);
-    });
-
-    console.log(sessionToken, "sessionToken");
-    console.log(pathname, "pathname");
+    const sessionToken = req.cookies.get(SESSION_KEY)?.value;
 
     if (protectedRoutes.some((path) => pathname.startsWith(path))) {
       if (!sessionToken) {
@@ -38,12 +26,7 @@ export const authorization: MiddlewareFactory = (next, _, data) => {
           return appError({ status: 401, error: "Invalid session" });
         }
 
-        console.log(user_id, "user_id");
-
         data.userId = user_id;
-        // const res = NextResponse.next();
-        // res.headers.set(HEADER_DATA_KEY, user_id);
-        // return res;
       } catch (error) {
         return appError({ status: 401, error: "Invalid session" });
       }
