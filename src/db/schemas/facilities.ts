a3b84e400216c380e708517aa448295be26740c2
henzyd <henzydee@gmail.com>
2024-12-12 17:28:18 +0100

    created POST and GET facility endpoint

--- src/db/schemas/facilities.ts
+++ src/db/schemas/facilities.ts
@@ -2,10 +2,8 @@ import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const facilitiesTable = pgTable("facilities", {
   id: uuid("id").primaryKey().defaultRandom(),
-  start_time: timestamp("start_time").notNull(),
-  end_time: timestamp("end_time").notNull(),
+  name: varchar("name", { length: 150 }).notNull().unique(),
   description: varchar("description"),
-  status: varchar("status", { enum: ["available", "unavailable"] }).notNull(),
   updated_at: timestamp("updated_at"),
   created_at: timestamp("created_at").defaultNow(),
 });
