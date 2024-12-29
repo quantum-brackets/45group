9dbfadd78992d8b8899cc09c76d77efda2b6e474
henzyd <henzydee@gmail.com>
2024-12-29 22:07:19 +0100

    updated services url

--- src/services/locations.ts
+++ src/services/locations.ts
@@ -3,13 +3,13 @@ import { Location } from "~/db/schemas/locations";
 
 class LocationsService {
   static createLocation = async (data: any) => {
-    const { data: response } = await axiosPrivate.postForm<Location>(`/api/locations`, data);
+    const { data: response } = await axiosPrivate.postForm<Location>(`/api/admin/locations`, data);
 
     return response;
   };
 
   static getLocations = async () => {
-    const { data: response } = await axiosPrivate.get<Location[]>(`/api/locations`);
+    const { data: response } = await axiosPrivate.get<Location[]>(`/api/admin/locations`);
 
     return response;
   };
