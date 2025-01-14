ec6a3a7d79b23fb89619abad5461506f0bf41261
henzyd <henzydee@gmail.com>
2025-01-14 05:31:28 +0100

    updated locaion service

--- src/services/locations.ts
+++ src/services/locations.ts
@@ -8,8 +8,12 @@ class LocationsService {
     return response;
   };
 
-  static getLocations = async () => {
-    const { data: response } = await axiosPrivate.get<Location[]>(`/api/admin/locations`);
+  static getLocations = async <T>({ params }: { params?: URLSearchParams } = {}) => {
+    const queryString = params ? `?${params.toString()}` : "";
+
+    const { data: response } = await axiosPrivate.get<
+      T extends Location[] ? T : Pagination<Location>
+    >(`/api/admin/locations${queryString}`);
 
     return response;
   };
