d5eb604674fa543d16996444009415c9a50bad58
henzyd <henzydee@gmail.com>
2025-01-21 03:09:54 +0100

    fix: time picker field

--- src/components/fields/time-picker-field.tsx
+++ src/components/fields/time-picker-field.tsx
@@ -3,11 +3,11 @@
 import { InputLabel, InputLabelProps } from "@mui/material";
 import { LocalizationProvider, TimePicker, TimePickerProps } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Dayjs } from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import { ErrorMessage, Field, FieldConfig, FieldProps } from "formik";
 import { cn } from "~/utils/helpers";
 
-type Props = TimePickerProps<Dayjs> &
+type Props = Omit<TimePickerProps<Dayjs>, "value" | "onChange"> &
   FieldConfig & {
     wrapperClassName?: string;
     labelProps?: InputLabelProps;
@@ -20,51 +20,64 @@ export default function TimePickerField({
   required,
   sx,
   label,
-  type,
-  format = "DD-MM-YYYY",
+  format = "hh:mm A",
   labelProps,
   ...props
 }: Props) {
   return (
     <Field {...props}>
-      {({ field, form }: FieldProps) => (
-        <div className={cn("flex w-full flex-col gap-1", wrapperClassName)}>
-          {label && (
-            <div className="flex w-full items-center justify-between">
-              <InputLabel
-                {...labelProps}
-                htmlFor={props.name}
-                className={cn(`!text-xs`, labelProps?.className)}
-              >
-                {label}
-                {required && <span className="pl-1 !text-xs !text-red-600">*</span>}
-              </InputLabel>
-            </div>
-          )}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <TimePicker
-              {...field}
-              {...props}
-              className={cn("w-full", className)}
-              sx={{
-                "& .MuiOutlinedInput-input": {
-                  padding: "11px 13.5px",
-                },
-              }}
-              disablePast
-              format={format}
-              onChange={(date: Dayjs | null) => {
-                date && form.setFieldValue(field.name, date.format(format));
-              }}
+      {({ field, form }: FieldProps) => {
+        // Convert the string value to Dayjs object for the TimePicker
+        const value = field.value ? dayjs(field.value, format) : null;
+
+        return (
+          <div className={cn("flex w-full flex-col gap-1", wrapperClassName)}>
+            {label && (
+              <div className="flex w-full items-center justify-between">
+                <InputLabel
+                  {...labelProps}
+                  htmlFor={props.name}
+                  className={cn(`!text-xs`, labelProps?.className)}
+                >
+                  {label}
+                  {required && <span className="pl-1 !text-xs !text-red-600">*</span>}
+                </InputLabel>
+              </div>
+            )}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                {...field}
+                {...props}
+                value={value}
+                className={cn("w-full", className)}
+                sx={{
+                  "& .MuiOutlinedInput-input": {
+                    padding: "11px 13.5px",
+                  },
+                  ...sx,
+                }}
+                disablePast
+                format={format}
+                ampm={true}
+                onChange={(newValue: Dayjs | null) => {
+                  const formattedTime = newValue?.format(format) || "";
+                  form.setFieldValue(field.name, formattedTime);
+                }}
+                slotProps={{
+                  textField: {
+                    error: Boolean(form.touched[field.name] && form.errors[field.name]),
+                  },
+                }}
+              />
+            </LocalizationProvider>
+            <ErrorMessage
+              name={props.name}
+              className={`pl-1 !text-xs !font-medium !text-red-600`}
+              component="p"
             />
-          </LocalizationProvider>
-          <ErrorMessage
-            name={props.name}
-            className={`pl-1 !text-xs !font-medium !text-red-600`}
-            component={"p"}
-          />
-        </div>
-      )}
+          </div>
+        );
+      }}
     </Field>
   );
 }
