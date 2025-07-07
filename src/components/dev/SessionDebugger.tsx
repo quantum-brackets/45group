a00261776b4e3947c8927f9590d5efcd244b63cf
Emmanuel Ogbizi-Ugbe <iamogbz@gmail.com>
2025-07-07 01:52:51 +0000

    Somehow the dev tools page still can not just render the stored token

--- src/components/dev/SessionDebugger.tsx
+++ src/components/dev/SessionDebugger.tsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { testLoginAction, verifySessionByIdAction } from "@/lib/actions";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
