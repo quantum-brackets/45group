875320fc557deff9fc4fb1d19c3b04eb0b11f4ef
Emmanuel Ogbizi-Ugbe <iamogbz@gmail.com>
2025-07-08 03:41:57 +0000

    Error: Invalid src prop (https://lh3.googleusercontent.com/fife/ALs6j_GS

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'avatar.vercel.sh',
         port: '',
         pathname: '/**',
